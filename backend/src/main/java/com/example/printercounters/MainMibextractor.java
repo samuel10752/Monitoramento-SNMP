1c61bd10346e13adf911929d70609d9579f5e800
samuel107152 <samuelcauabarbosa114@gmail.com>
2025-03-09 00:06:41 -0300

    Codigo para Executar a interface para extratir a mib da impressora, fase de teste

---
+++ backend/src/main/java/com/example/printercounters/MainMibextractor.java
@@ -0,0 +1,11 @@
+package com.example.printercounters;
+
+import com.example.printercounters.MIBExtractor.MIBExtractorGUI;
+import javafx.application.Application;
+
+public class MainMibextractor {
+    public static void main(String[] args) {
+        // Executa a interface gráfica do MIB Extractor
+        Application.launch(MIBExtractorGUI.class, args);
+    }
+}
