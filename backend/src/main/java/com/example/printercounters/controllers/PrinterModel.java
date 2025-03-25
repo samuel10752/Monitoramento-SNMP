e5ae86e36a65e5218424ec3f1b82ed9191673305
samuel107152 <samuelcauabarbosa114@gmail.com>
2025-03-25 14:10:17 -0300

    V.2.0 Adicionado um novo função para pegar os ips e informações corretamente

--- backend/src/main/java/com/example/printercounters/controllers/PrinterModel.java
+++ backend/src/main/java/com/example/printercounters/controllers/PrinterModel.java
@@ -8,18 +8,10 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-import org.snmp4j.CommunityTarget;
-import org.snmp4j.PDU;
-import org.snmp4j.Snmp;
-import org.snmp4j.event.ResponseEvent;
-import org.snmp4j.smi.OID;
-import org.snmp4j.smi.VariableBinding;
-
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public abstract class PrinterModel {
-
     protected String ip;
     protected TextField macField;
     protected TextField serialField;
@@ -35,6 +27,14 @@ public abstract class PrinterModel {
         this.webInfoArea = webInfoArea;
     }
 
+    // Abstract method for subclasses to override
+    public abstract void fetchOidData(String oid);
+
+    // Getter for IP
+    public String getIp() {
+        return ip;
+    }
+
     public abstract String getMacAddress();
 
     public abstract String getSerialNumber();
@@ -45,7 +45,7 @@ public abstract class PrinterModel {
 
     public abstract void fetchWebPageData();
 
-    public static void disableSSLCertificateChecking() {
+    public void disableSSLCertificateChecking() {
         try {
             TrustManager[] trustAllCertificates = new TrustManager[] {
                     new X509TrustManager() {
@@ -68,21 +68,5 @@ public abstract class PrinterModel {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    protected String getSnmpValue(String oid, Snmp snmp, CommunityTarget<?> target) {
-        try {
-            PDU pdu = new PDU();
-            pdu.add(new VariableBinding(new OID(oid)));
-            pdu.setType(PDU.GET);
-
-            ResponseEvent response = snmp.get(pdu, target);
-            if (response != null && response.getResponse() != null) {
-                return response.getResponse().get(0).getVariable().toString();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "Desconhecido";
     }
 }
